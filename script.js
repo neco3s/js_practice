4f2ee278c657b6c01f87a0aa443caff91965e373
neco3s <neco3coffee@gmail.com>
2023-04-22 22:54:25 +0900

    jsを手を動かして学んだ軌跡

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// const n = 4;
+// switch(n) {
+//   case 1:
+//     console.log('one');
+//     break;
+//   case 2:
+//     console.log('two');
+//     break;
+//   default:
+//     console.log('other');
+//     break;
+// }
+
+
+// let age =9;
+
+// if (age >= 12) {
+//   console.log('5000yen');
+// } else if (age >=6) {
+//   console.log('2500yen');
+// } else {
+//   console.log('1000yen');
+// }
+
+// let sum =0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+
+// const scores = [100,90,80,70,60];
+// let sum = 0;
+// for (let i = 0;i < scores.length; i++) {
+//   sum += scores[i];
+// }
+
+// const average = sum / scores.length;
+// console.log(sum);
+// console.log(average);
+
+// const get = (height,width) => {
+//   return height * width;
+// }
+
+// console.log(get(3,5))
+
+
+// const calc = (price,num) => {
+//   return price * num;
+// }
+// console.log(calc(100,13));
+
+// const e = document.getElementById('button');
+// const list = document.getElementById('list');
+// e?.addEventListener('click', () => {
+//   const li = document.createElement('li');
+//   const text = document.createTextNode('text');
+//   li.appendChild(text);
+//   list?.appendChild(li);
+// },false);
+
+window.onload = () => {
+  console.log('hello')
+}
